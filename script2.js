4186eba9eea9fddf5d00c2b455c83e5d8cfee14f
Talha Masood <81777772+talhamasood642@users.noreply.github.com>
2021-09-04 15:47:43 -0700

    new changes

---
+++ script2.js
@@ -0,0 +1,6 @@
+const login = () => {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    console.log(name, email ,password);
+}
