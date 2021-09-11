0c999dfddaa235c8261bb6716038a95109c993d4
Talha Masood <81777772+talhamasood642@users.noreply.github.com>
2021-09-10 23:56:13 -0700

    new project

---
+++ Talha project/firebase.js
@@ -0,0 +1,13 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBuqqrbTrxssFlF9z6I723ZaA4i-FsYKpk",
+//     authDomain: "fir-project-1afb7.firebaseapp.com",
+//     projectId: "fir-project-1afb7",
+//     storageBucket: "fir-project-1afb7.appspot.com",
+//     messagingSenderId: "263428483415",
+//     appId: "1:263428483415:web:f5b1166f90f415b805f612",
+//     measurementId: "G-B6MF0Z68ZG"
+//   };
+
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);            
